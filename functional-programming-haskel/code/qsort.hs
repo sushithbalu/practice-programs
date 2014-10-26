e855d8d7579b2f72e1231ad2a28971ec025e9c7e
sushithbalu <sushith1132@gmail.com>
2014-10-26 18:49:33 +0530

    update1

--- functional-programming-haskel/code/qsort.hs
+++
@@ -1,9 +0,0 @@
-f [] = []
-f (x:xs) = f sm ++ [x] ++ f la
-	where
-		sm = [sm | sm <-xs, sm <= x]
-		la = [la | la <-xs, la > x]
-{-large element = [xs list , condition(la > head]--which is a list -}
-{-
--> less efficient and slow, as compared to #c code
--}
